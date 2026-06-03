8545a54aa6e1400ecaa2a8549c3b8d24488e7834
Intisy <finn@birich.de>
2026-06-03 18:43:15 +0200

    chore: remove BOM from files introduced by PowerShell

--- plugin.js
+++ plugin.js
@@ -1,4 +1,4 @@
-﻿import { existsSync, writeFileSync, mkdirSync, readFileSync } from "fs";
+import { existsSync, writeFileSync, mkdirSync, readFileSync } from "fs";
 import { join } from "path";
 import { homedir } from "os";
 
